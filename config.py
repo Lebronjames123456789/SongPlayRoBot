ab9f50520d12176f3c74ccbcf98d1eea866ac85d
Lebronjames123456789 <abdinasirnursalad@gmail.com>
2024-09-09 20:09:29 +0300

    Lebrojamesmusicbot
    Lebronjamesmusicbot

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "20897060"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "245d1f187ddefa2d9c46bb9e37cf4557" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "7254926762:AAGfzaQN9CxtNepE5MxWt4JPd5FDmwzArU8" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
